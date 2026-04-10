1c8750452cde21ea28863968f40935f746b62925
Zhang Ruixuan <81840032+zahra666@users.noreply.github.com>
2026-04-10 15:59:46 +0800

    Update 3-8.py

--- chapter03/3-8.py
+++ chapter03/3-8.py
@@ -1,3 +1,9 @@
-tours = ['Hangzhou','Shanghai','Wuhan','Suzhou','Kunming']
+tours = ['Tokyo','Shanghai','Wuhan','Suzhou','Kunming']
 print(tours)  #原始打印
 print(sorted(tours))  #按字母顺序打印
+print(sorted(tours, reverse=True))
+print(tours)
+tours.reverse()
+print(tours)
+print(tours.sort())
+print(tours.sort(reverse=True))
