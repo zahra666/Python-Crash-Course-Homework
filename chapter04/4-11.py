7b33bcb35c7bb5bd66fb6b505c39adf7cfc736f6
Zhang Ruixuan <81840032+zahra666@users.noreply.github.com>
2026-04-12 19:32:48 +0800

    4-11.py

--- chapter04/4-11.py
+++ chapter04/4-11.py
@@ -4,13 +4,13 @@ friend_pizzas = pizza_list[:]
 pizza_list.append('blue pizza')
 friend_pizzas.append('black pizza')
 
-print("My pizza:")
 print(pizza_list)
-print("\n Friend pizza:")
 print(friend_pizzas)
 
+print("My favorite pizzas are:")
 for myp in pizza_list:
     print(myp)
-
+        
+print("\n My friend's favorite pizzas are:")
 for frp in friend_pizzas:
     print(frp)
