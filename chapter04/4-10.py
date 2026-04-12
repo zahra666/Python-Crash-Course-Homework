18de86f25eb3ba3484b45d51117e4b2d9af749d5
Zhang Ruixuan <81840032+zahra666@users.noreply.github.com>
2026-04-12 19:18:46 +0800

    4-10.py

--- chapter04/4-10.py
+++ chapter04/4-10.py
@@ -1,5 +1,14 @@
-msg1 = ['The','first','three','items','in','the','list','are:']
-print(msg1)
-print(msg1[0:3])
-print(msg1[3:6])
-print(msg1[-3:])
+my_foods = ['pizza', 'falafel', 'carrot cake', 'cannoli', 'ice cream', 'burger', 'sushi']
+
+print("The first three items in the list are:")
+print(my_foods[:3])
+
+# 确定中间是哪个索引号
+# my_foods[mid-1 : mid+2]
+mid = len(my_foods) // 2
+
+print("\nThree items from the middle of the list are:")
+print(my_foods[2:5])
+
+print("\nThe last three items in the list are:")
+print(my_foods[-3:])
