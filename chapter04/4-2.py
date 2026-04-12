ddd1136de91d2fc12fb473a7e8c20f5de6557a40
Zhang Ruixuan <81840032+zahra666@users.noreply.github.com>
2026-04-12 09:42:46 +0800

    4-2.py

--- chapter04/4-2.py
+++ chapter04/4-2.py
@@ -1,6 +1,6 @@
 animails = ['cat','dog','snake']
 for animail in animails:
     print(animail)
-    print("A " + animail.title() + " would make a great pet")
+    print("A " + animail+ " would make a great pet")
 
 print("Any of these animals would make a great pet!")
